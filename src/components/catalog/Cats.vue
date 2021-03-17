586144a34f73e987e8eb0b4960b91d091112f649
bo <bobiora@gmail.com>
2021-03-17 17:43:04 +0200

    update v2.1.5

--- src/components/catalog/Cats.vue
+++ src/components/catalog/Cats.vue
@@ -6,9 +6,14 @@
       >Cart: {{ CART.length }}</router-link
     >
     <!-- <h3>{{ title }}</h3> -->
+    <Select
+      :options="categories"
+      @select="sort"
+      :selected="selected"
+    />
     <div class="cats-list">
       <CatsItem
-        v-for="product in PRODUCTS"
+        v-for="product in filtered"
         :key="product.id"
         :product_data="product"
         @addToCart="addToCart"
@@ -19,23 +24,51 @@
 <script>
 import CatsItem from "@/components/catalog/CatsItem.vue";
 import { mapGetters, mapActions } from "vuex";
+import Select from "@/components/catalog/Select.vue"
 
 export default {
   name: "Cats",
   components: {
-    CatsItem
+    CatsItem,
+    Select
   },
   props: {},
   data() {
-    return { title: "Cats" };
+    return {
+            title: "Cats",
+            categories: [
+              {name: "All Arts", value: "all"},
+              {name: "Framed Prints", value: "framed"},
+              {name: "Mini Art Prints", value: "mini"},
+            ],
+            selected: "Filter",
+            sortedProducts: []
+          }
   },
   computed: {
-    ...mapGetters(["PRODUCTS", "CART"])
+    ...mapGetters(["PRODUCTS", "CART"]),
+    filtered(){
+      if(this.sortedProducts.length){
+        return this.sortedProducts
+      }else{
+        return this.PRODUCTS
+      }
+    }
   },
   methods: {
     ...mapActions(["GET_PRODUCTS_FROM_API", "ADD_TO_CART"]),
     addToCart(data) {
       this.ADD_TO_CART(data);
+    },
+    sort(category){
+      this.sortedProducts = [];
+      let sortMap = this;
+      this.PRODUCTS.map(function(item){
+        if(item.category === category.value){
+          sortMap.sortedProducts.push(item);
+        }
+      });
+      this.selected = category.name
     }
   },
   watch: {},
---
+++ src/components/catalog/Select.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="select">
+        <p
+            class="select__title"
+            @click="areOpVisible = !areOpVisible"
+        >{{selected}}</p>
+        <div
+            class="select__options"
+            v-if="areOpVisible"
+        >
+            <p
+                class="select__option"
+                v-for="option in options"
+                :key="option.value"
+                @click="selectOption(option)"
+            >
+                {{option.name}}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Select",
+    props: {
+        options: {
+            type: Array,
+            default(){
+                return []
+            }
+        },
+        selected: {
+            type: String,
+            default: ""
+        }
+    },
+    data() {
+        return {
+            areOpVisible: false
+        }
+    },
+    methods: {
+        selectOption(option) {
+            this.$emit('select', option);
+            this.areOpVisible = false;
+        },
+        hideSelect(){
+            this.areOpVisible = false;
+        }
+    },
+    mounted() {
+        document.addEventListener("click", this.hideSelect.bind(this), true);
+    },
+    beforeDestroy() {
+        document.removeEventListener("click", this.hideSelect);
+    }
+
+}
+</script>
+<style lang="sass">
+    .select
+        position: relative
+        width: 280px
+        margin-bottom: 30px
+        &__title
+            font-weight: bold
+            padding: 3px
+            border: 1px solid #000
+            cursor: pointer
+        &__options
+            background-color: #fff
+            border: 1px solid #000
+            position: absolute
+            left: 0
+            top: 100%
+            margin-top: -1px
+            width: 100%
+        &__option
+            cursor: pointer
+            padding: 5px
+            &:hover
+                background-color: #ebebeb
+            &:not(:last-child)
+                border-bottom: 1px solid #000
+
+</style>
